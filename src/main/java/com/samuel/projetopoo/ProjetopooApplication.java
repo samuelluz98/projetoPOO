789a0ad55fbc7d8f37789eabe5b684926f087d58
José Samuel <jose.samuel@dce.ufpb.br>
2019-08-24 20:02:06 -0300

    Projeto criado

---
+++ src/main/java/com/samuel/projetopoo/ProjetopooApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.projetopoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetopooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetopooApplication.class, args);
+	}
+
+}
